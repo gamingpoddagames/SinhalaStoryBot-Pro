287773406c9d3a9a1716b4b4bdf45a878345a1d9
CoreFlow <gamingpoddagames@gmail.com>
2026-08-05 01:31:50 +0530

    Update scrolling_text.py

--- src/subtitle/scrolling_text.py
+++ src/subtitle/scrolling_text.py
@@ -1,37 +1,23 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
-import textwrap
 
 
 def get_font(size):
 
     fonts = [
-
         "/usr/share/fonts/opentype/noto/NotoSansSinhala-Regular.ttf",
-
-        "/usr/share/fonts/truetype/noto/NotoSansSinhala-Regular.ttf",
-
-        "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
-
+        "/usr/share/fonts/truetype/noto/NotoSansSinhala-Regular.ttf"
     ]
 
-
-    for font in fonts:
-
-        if os.path.exists(font):
-
-            return ImageFont.truetype(
-                font,
-                size
-            )
-
+    for f in fonts:
+        if os.path.exists(f):
+            return ImageFont.truetype(f,size)
 
     return ImageFont.load_default()
 
 
 
 def create_text_image(text, filename):
-
 
     os.makedirs(
         "output/temp",
@@ -53,37 +39,44 @@ def create_text_image(text, filename):
     draw = ImageDraw.Draw(img)
 
 
-    # smaller but clear text
-
-    font = get_font(65)
+    font = get_font(55)
 
 
 
-    # split Sinhala text into lines
+    # split text into smaller parts
 
-    lines = []
-
-
-    for paragraph in text.split("\n"):
-
-        wrapped = textwrap.wrap(
-            paragraph,
-            width=18
-        )
-
-        lines.extend(
-            wrapped
-        )
+    words = text.split()
 
 
+    lines=[]
 
-    # center alignment
-
-    y = 700
+    current=""
 
 
-    for line in lines:
+    for word in words:
 
+        if len(current + word) < 22:
+
+            current += word + " "
+
+        else:
+
+            lines.append(current)
+
+            current = word + " "
+
+
+    if current:
+        lines.append(current)
+
+
+
+    # show more lines
+
+    y=550
+
+
+    for line in lines[:10]:
 
         box = draw.textbbox(
             (0,0),
@@ -91,77 +84,27 @@ def create_text_image(text, filename):
             font=font
         )
 
+        x=(width-(box[2]-box[0]))//2
 
-        text_width = (
-            box[2]-box[0]
-        )
-
-
-        x = (
-            width-text_width
-        )//2
-
-
-
-        # shadow
 
         draw.text(
-
-            (
-                x+4,
-                y+4
-            ),
-
+            (x,y),
             line,
-
             font=font,
-
-            fill=(0,0,0,220)
-
+            fill="white",
+            stroke_width=3,
+            stroke_fill="black"
         )
 
 
-
-        # main text
-
-        draw.text(
-
-            (
-                x,
-                y
-            ),
-
-            line,
-
-            font=font,
-
-            fill=(255,255,255,255),
-
-            stroke_width=2,
-
-            stroke_fill=(0,0,0,255)
-
-        )
-
-
-        y += 100
+        y+=90
 
 
 
-    path = (
-
-        "output/temp/"
-
-        + filename
-
-        + ".png"
-
-    )
+    path=f"output/temp/{filename}.png"
 
 
-    img.save(
-        path
-    )
+    img.save(path)
 
 
     return path
