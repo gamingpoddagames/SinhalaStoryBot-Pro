1b48b6bd6a9d422f10f2b9fa6d89b9aab5e4fd5c
CoreFlow <gamingpoddagames@gmail.com>
2026-08-04 22:07:56 +0530

    Add upload_facebook function for video uploads

---
+++ src/upload/facebook.py
@@ -0,0 +1,13 @@
+def upload_facebook(video, metadata):
+
+    print(
+        "Facebook Reel Ready:"
+    )
+
+    print(video)
+
+    print(metadata)
+
+    # Add Facebook Graph API later
+
+    return True
