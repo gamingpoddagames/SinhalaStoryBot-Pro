9aece1893bc6a6a4b1a3e5110759e74b809f12ea
CoreFlow <gamingpoddagames@gmail.com>
2026-08-04 22:07:31 +0530

    Create youtube.py

---
+++ src/upload/youtube.py
@@ -0,0 +1,13 @@
+def upload_youtube(video, metadata):
+
+    print(
+        "YouTube Upload Ready:"
+    )
+
+    print(video)
+
+    print(metadata)
+
+    # Add YouTube API here later
+
+    return True
